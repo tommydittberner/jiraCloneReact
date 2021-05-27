f1048b0496e6720aaeceef96f1e0bcabd2dada15
Tommy Dittberner <todi@ITFSPACE.NET>
2021-05-27 07:33:32 +0200

    fixed saving theme to localStorage

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -4,10 +4,8 @@ const useTheme = () => {
     const [theme, setTheme] = useState('light')
 
     useEffect(() => {
-       const storedTheme = window.localStorage.getItem('components/theme')
-       if (storedTheme) {
-           setTheme(storedTheme);
-       }
+       const storedTheme = window.localStorage.getItem('theme')
+       storedTheme && setTheme(storedTheme);
     }, []);
 
     const toggleTheme = () => {
